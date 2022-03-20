235d03af6d8fed33ec01a7b4a265732e0ad2bb37
pramsham <93697916+pramsham@users.noreply.github.com>
2022-03-20 11:15:31 +0530

    Add files via upload

---
+++ maximumproductarray.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/maximum-product-subarray/submissions/
+class Solution:
+    def maxProduct(self,A: List[int]) -> int:
+        B = A[::-1]
+        for i in range(1,len(A)):
+            if A[i-1] != 0:
+                A[i]*=A[i-1]
+            if B[i-1] != 0:
+                B[i]*=B[i-1]
+        return max(A+B)
